f7c3f3f4594e9de26d83606ee4b064ea41579c23
Philipp Jacoby <overflowerror@gmx.de>
2025-09-17 11:12:54 +0200

    again

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "TerraOrga"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+
+resource "azurerm_resource_group" "MyRG" {
+  location = "westus2"
+  name     = "Azure Devops Main"
+
+}
